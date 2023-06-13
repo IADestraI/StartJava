4a983f317e2a2f0b48b5d6e8e89a9b8cae70bc82
IADestraI <adestra1337@gmail.com>
2023-06-13 17:47:19 +0300

    implement hw_2

---
+++ Lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,188 @@
+public class IfElseStatementTheme    {
+    public static void main(String[] args) {
+        // 1. Перевод псевдокода на язык Java //
+        System.out.println("\n1.Перевод псевдокода на язык Java");
+
+        int age = 25;
+        if(age > 20) {
+            System.out.println("Возраст больше 20");
+        } else {
+            System.out.println("Возраст меньше 20");
+        }
+
+        boolean male = true;
+        if(!male) {
+            System.out.println("Пол женский");
+        } else {
+            System.out.println("Пол мужской");
+        }
+
+        double height = 1.75;
+        if(height < 1.80) {
+            System.out.println("Рост ниже 1.80");
+        } else {
+            System.out.println("Рост выше 1.80");
+        }
+
+        char firstLetterName = "Anton".charAt(0);
+        if(firstLetterName == 'M') {
+            System.out.println("Первая буква имени M");
+        } else if(firstLetterName == 'I') {
+            System.out.println("Первая буква имени I");
+        } else {
+            System.out.println("Имя начинается не с M или I");
+        }
+
+        // 2. Поиск max и min числа//
+        System.out.println("\n2. Поиск max и min числа");
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Число 1 = " + num1);
+        System.out.println("Число 2 = " + num2);
+
+        // Поиск максимального числа//
+        int maxNum = Math.max(num1, num2);
+        System.out.println("Максимальное число = " + maxNum);
+        // Поиск минимального числа //
+        int minNum = Math.min(num1, num2);
+        System.out.println("Минимальное число = " + minNum);
+        // Проверка равенства чисел//
+        if(num1 == num2) {
+            System.out.println("Число 1 и число 2 равны");
+        } else {
+            System.out.println("Число 1 и число 2 неравны");
+        }
+
+        // 3. Проверка числа //
+        System.out.println("\n3.Проверка числа");
+        int num3 = 0;
+        System.out.println("Проверка числа " + num3);
+        // проверка числа //
+        if(num3 == 0) {
+            System.out.println("Число равняется нулю");
+        } else {
+            if(num3 % 2 == 1) {
+                System.out.println("Число нечетное");
+            }   else {
+                System.out.println("Число четное");
+            }
+            if(num3 > 0) {
+                System.out.println("Число положительное");
+            } else {
+                System.out.println("Число отрицательное");
+            }
+        }
+        
+        
+
+        // 4. Поиск одинаковых цифр в числах//
+        System.out.println("\n4. Поиск одинаковых цифр в числах");
+        int num4 = 123;
+        int num5 = 223;
+        System.out.println("Исходные числа: " + num4 + " и " + num5);
+        int ones1 = num4 % 10;
+        int ones2 = num5 % 10;
+        int tens1 = (num4 / 10) % 10;
+        int tens2 = (num5 / 10) % 10;
+        int hundreds1 = num4 / 100;
+        int hundreds2 = num5 / 100;
+        if (ones1 == ones2) {
+            System.out.println("Одинаковая цифра: " + ones1);
+            System.out.println("Номер разряда: единицы");
+        } 
+        if (tens1 == tens2) {
+            System.out.println("Одинаковая цифра: " + tens1);
+            System.out.println("Номер разряда: десятки");
+        }
+        if (hundreds1 == hundreds2) {
+            System.out.println("Одинаковая цифра: " + hundreds1);
+            System.out.println("Номер разряда: сотни");
+        }
+        if (ones1 != ones2 && tens1 != tens2 && hundreds1 != hundreds2) {
+            System.out.println("Нет одинаковых цифр в одинаковых разрядах.");
+        }
+
+        // 5. Определение символа по его коду //
+        System.out.println("\n5. Определение символа по его коду");
+        char symbol = '\u0057';
+        System.out.println("Символ: " + symbol);
+        if (symbol >= 'a' && symbol <= 'z') {
+            System.out.println("Маленькая буква");
+        } else if (symbol >= 'A' && symbol <= 'Z') {
+            System.out.println("Большая буква");
+        } else if (symbol >= '0' && symbol <= '9') {
+            System.out.println("Число");
+        } else {
+            System.out.println("Не буква и не число");
+        }
+
+        // 6. Подсчет суммы вклада и начисленных банком % //
+        System.out.println("\n6.Подсчет суммы вклада и начисленных банком %");
+        double sumDeposit = 300_000;
+        System.out.println("Сумма вклада: " + sumDeposit);
+        double accruedInterest; 
+        if (sumDeposit < 100_000) {
+            accruedInterest = sumDeposit * 0.05;
+        } else if (sumDeposit >= 100_000 && sumDeposit <= 300_000) {
+            accruedInterest = sumDeposit * 0.07;
+        } else {
+            accruedInterest = sumDeposit * 0.1;
+        }
+        System.out.println("Начисленные проценты: " + accruedInterest);
+        double totalSum = sumDeposit + accruedInterest;
+        System.out.println("Итоговая сумма с процентами: " + totalSum);
+
+        // 7.Определение оценки по предметам //
+        System.out.println("\n7.Определение оценки по предметам");
+        int percentageHistory = 59;
+        int percentageProgramming = 91;
+
+        int gradeHistory;
+        int gradeProgramming;
+
+        if (percentageHistory <= 60) {
+            gradeHistory = 2;
+        } else if (percentageHistory > 60 && percentageHistory <= 73) {
+            gradeHistory = 3;
+        } else if (percentageHistory > 73 && percentageHistory <= 91) {
+            gradeHistory = 4;
+        } else {
+            gradeHistory = 5;
+        }
+
+        if (percentageProgramming <= 60) {
+            gradeProgramming = 2;
+        } else if (percentageProgramming > 60 && percentageProgramming <= 73) {
+            gradeProgramming = 3;
+        } else if (percentageProgramming > 73 && percentageProgramming <= 91) {
+            gradeProgramming = 4;
+        } else {
+            gradeProgramming = 5;
+        }
+
+        System.out.println("Оценки по предметам:");
+        System.out.println("История " + gradeHistory);
+        System.out.println("Программирование " + gradeProgramming);
+
+        int averageGrade = (gradeProgramming + gradeHistory) / 2;
+        System.out.println("Средний балл оценок по предметам " + averageGrade);
+
+        int averagePercentage = (percentageHistory + percentageProgramming) / 2;
+        System.out.println("Средний % по предметам " + averagePercentage);
+
+        // 8. Расчет прибыли за год //
+        System.out.println("\n8. Расчет прибыли за год");
+        int rentalPrice = 5000;
+        int averageSales = 13000;
+        int costPrice = 9000;
+
+        int annualProfit = ((averageSales - costPrice) - rentalPrice) * 12;
+        if (annualProfit > 0) {
+            System.out.println("Прибыль за год: +" + annualProfit + " руб.");
+        } else if (annualProfit < 0) {
+            System.out.println("Прибыль за год: " + annualProfit + " руб.");
+        } else {
+            System.out.println("Прибыль за год: 0 руб.");
+        }
+    }
+}
---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,141 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        // 1. Вывод характеристик компьютера//
+        System.out.println("\n1. Вывод характеристик компьютера");
+        byte cores = 16;
+        short memory = 32;
+        int graphCard = 3070;
+        long mBoard = 690;
+        float freqCpu = 4.2f;
+        double freqRam = 4800;
+        char chipMBoard = 'Z';
+        boolean wifi = true;
+
+        System.out.println("cores = " + cores);
+        System.out.println("memory = " + memory);
+        System.out.println("graphCard = " + graphCard);
+        System.out.println("mBoard = " + mBoard);
+        System.out.println("freqCPU = " + freqCpu);
+        System.out.println("freqRam = " + freqRam);
+        System.out.println("chipMBoard = " + chipMBoard);
+        System.out.println("wifiMBoard = " + wifi);
+
+        // 2. Расчет стоимости товара со скидкой//
+        int pricePen = 100;
+        int priceBook = 200;
+        int price = pricePen + priceBook;
+        float discountPrice = price * 0.11f;
+
+        System.out.println("\n2. Расчет стоимости товара со скидкой");
+        System.out.println("общая стоимость товаров без скидки = " + price);
+        System.out.println("сумма скидки (11%) = " + discountPrice);
+        System.out.print("общая стоимость товаров со скидкой = ");
+        System.out.println(price - discountPrice);
+
+        // 3. Вывод слова JAVA//
+        System.out.println("\n3. Вывод слова JAVA");
+        System.out.println("   J    a  v     v  a        ");
+        System.out.println("   J   a a  v   v  a a     ");
+        System.out.println("J  J  aaaaa  V V  aaaaa           ");
+        System.out.println(" JJ  a     a  V  a     a\n");
+
+        // 4. Вывод min и max значений целых числовых типов //
+        System.out.println("4. Вывод min и max значений целых числовых типов");
+        byte byteMax = 127;
+        System.out.println("Первоначальное значение byte = " + byteMax);
+        byteMax++;
+        System.out.println("Инкремент byte = " + byteMax);
+        byteMax--;
+        System.out.println("Декремент byte = " + byteMax);
+        short shortMax = 32767;
+        System.out.println("Первоначальное значение short = " + shortMax);
+        shortMax++;
+        System.out.println("Инкремент short = " + shortMax);
+        shortMax--;
+        System.out.println("Декремент short = " + shortMax);
+        int intMax = 2147483647;
+        System.out.println("Первоначальное значение int = " + intMax);
+        intMax++;
+        System.out.println("Инкремент int = " + intMax);
+        intMax--;
+        System.out.println("Декремент int = " + intMax);
+        long longMax = 922337203685477580L;
+        System.out.println("Первоначальное значение byte = " + longMax);
+        longMax++;
+        System.out.println("Инкремент int = " + longMax);
+        longMax--;
+        System.out.println("Декремент int = " + longMax);
+
+        // 5. Перестановка значений переменных // 
+        System.out.println("\n5. Перестановка значений переменных");
+        int a = 2;
+        int b = 5;
+        
+        // I.с помощью третьей переменной //
+        System.out.println("I. С помощью третьей переменной\n"+ a);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(b);
+
+        // II.с помощью арифметических операций //
+        System.out.println("II. С помощью арифметических операций\n" + a);
+        a += b;
+        b = a - b;
+        a -= b;
+        System.out.println(b);
+
+        // III.c помощью побитовой операции ^ //
+        System.out.println("III. C помощью побитовой операции ^\n" + a);
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println(b);
+
+        // 6.Вывод символов и их кодов //
+        System.out.println("\n6.Вывод символов и их кодов");
+        char sign ='#';
+        char ampersand ='&';
+        char at ='@';
+        char circumflexus ='^';
+        char lowLine ='_';
+        System.out.println( ((int) sign) + " = " + sign);
+        System.out.println( ((int) ampersand) + " = " + ampersand);
+        System.out.println( ((int) at) + " = " + at);
+        System.out.println( ((int) circumflexus) + " = " + circumflexus);
+        System.out.println( ((int) lowLine) + " = " + lowLine);
+
+        // 7.Вывод в консоль ASCII-арт Дюка //
+        System.out.println("\n7.Вывод в консоль ASCII-арт Дюка");
+        char backslash = '\\';
+        char slash = '/';
+        char bracket = '(';
+        char bracket1 = ')';
+        System.out.println("    " + slash + backslash);
+        System.out.println("   " + slash + "  " + backslash);
+        System.out.println("  " + slash + lowLine + bracket + " " + bracket1 + backslash);
+        System.out.println(" " + slash + "      " + backslash);
+        System.out.println("" + slash + lowLine + lowLine + lowLine + lowLine + slash + backslash + lowLine + lowLine + backslash);
+        
+        // 8.Вывод количества сотен, десятков и единиц числа //
+        System.out.println("\n8.Вывод количества сотен, десятков и единиц числа");
+        int number = 123;
+        int hundreds = number / 100;
+        int tens = (number % 100) / 10;
+        int ones = number % 10;
+        System.out.println("Число 123 содержит:");
+        System.out.println(hundreds + " Сотен");
+        System.out.println(tens + " Десятков");
+        System.out.println(ones + " Единиц");
+        System.out.println("Сумма его цифр  = " + (hundreds + tens + ones));
+        System.out.println("Произведение его цифр = " + hundreds * tens * ones);
+
+        // 9.Вывод времени //
+        System.out.println("\n9.Вывод времени");
+        int totalSeconds = 86399;
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
+    }
+}
